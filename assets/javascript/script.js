e1e4275b0d1329c3bc8def74d051942bb58162f8
Daniel Semple <dsemple.isu@gmail.com>
2020-08-19 12:54:00 -0500

    added prompt for button later

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,8 @@
 const container = document.querySelector("#container");
+const gridSize = 600;
+const a = prompt("Size of grid?")
 console.log(container);
-createGrid();
+createGrid(a);
 
 function createGrid(a = 16) {
     const mainGrid = document.createElement("div");
@@ -9,6 +11,8 @@ function createGrid(a = 16) {
         for (let col = 0; col < a; col++) {
             const box = document.createElement("div");
             box.classList.add("box");
+            box.style.width = gridSize / a + 'px';
+            box.style.height = gridSize / a + 'px';
             box.addEventListener("mouseover", fillbox);
             rowDiv.appendChild(box);
         }
