037176a9e8d6b69126b67978d2f006174258b3f1
Daniel Semple <dsemple.isu@gmail.com>
2020-08-19 21:08:26 -0500

    updated to add reset button and clearGrid function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,7 @@
 const container = document.querySelector("#container");
 const gridSize = 600;
-const a = prompt("Size of grid?")
 console.log(container);
-createGrid(a);
+createGrid();
 
 function createGrid(a = 16) {
     const mainGrid = document.createElement("div");
@@ -14,10 +13,23 @@ function createGrid(a = 16) {
             box.style.width = gridSize / a + 'px';
             box.style.height = gridSize / a + 'px';
             box.addEventListener("mouseover", fillbox);
+            box.addEventListener("click", fillbox);
             rowDiv.appendChild(box);
         }
         container.appendChild(rowDiv);
     }
+}
+
+function clearGrid() {
+
+    while (container.firstElementChild) { 
+        container.removeChild(container.firstElementChild); 
+    }
+    
+    // const filledBoxes = document.querySelectorAll(".filledBox");
+    // filledBoxes.forEach(box => box.classList.remove("filledBox"));
+    const a = prompt("Size of new grid?");
+    createGrid(a);
 }
 
 function fillbox(e) {
