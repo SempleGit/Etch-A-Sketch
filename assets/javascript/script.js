76a1e4ffae7e3cd36d91cd99fce3253007ae6a07
Daniel Semple <dsemple.isu@gmail.com>
2020-08-20 09:08:12 -0500

    added color changing to the boxes

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,5 @@
 const container = document.querySelector("#container");
 const gridSize = 600;
-console.log(container);
 createGrid();
 
 function createGrid(a = 16) {
@@ -8,12 +7,16 @@ function createGrid(a = 16) {
     for (let row = 0; row < a; row++) {
         const rowDiv = document.createElement("div");
         for (let col = 0; col < a; col++) {
-            const box = document.createElement("div");
+
+            const box = document.createElement('div', {
+                is: 'grid-box'
+            });
+            
             box.classList.add("box");
             box.style.width = gridSize / a + 'px';
             box.style.height = gridSize / a + 'px';
             box.addEventListener("mouseenter", fillbox);
-            box.addEventListener("click", fillbox);
+            box.addEventListener("ontouchmove", fillbox);
             rowDiv.appendChild(box);
         }
         container.appendChild(rowDiv);
@@ -21,11 +24,9 @@ function createGrid(a = 16) {
 }
 
 function clearGrid() {
-
     while (container.firstElementChild) { 
         container.removeChild(container.firstElementChild); 
     }
-    
     // const filledBoxes = document.querySelectorAll(".filledBox");
     // filledBoxes.forEach(box => box.classList.remove("filledBox"));
     const a = prompt("Size of new grid?");
@@ -33,7 +34,27 @@ function clearGrid() {
 }
 
 function fillbox(e) {
+    this.count++;
     if (!e.target.classList.contains("filledBox")) {
         e.target.classList.add("filledBox");
     }
+    if (this.count >= 3) {
+        e.target.style.backgroundColor = getRandomColor();
+    }
 }
+
+function getRandomColor() {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    return "#" + randomColor;
+}
+
+class Box extends HTMLDivElement {   
+    constructor() {
+        super();
+        this.count = 0;
+    }
+}
+
+customElements.define('grid-box', Box, {
+    extends: "div"
+});
